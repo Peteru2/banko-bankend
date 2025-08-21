730ca0bd9f825c455316e21e484cc09b3edc67d9
Olatunji Peter <63746720+Peteru2@users.noreply.github.com>
2025-08-21 13:32:11 +0100

    code reformed

--- db.js
+++ db.js
@@ -1,15 +1,17 @@
 
 const mongoose = require('mongoose');
 require('dotenv').config();
+// const url = "mngodb+srv://banko:mybanko123@cluster0.to9arvf.mongodb.net/";
 const url = process.env.DB_URL;
 
-const connectDB = async () => {
+
+async function connectDB() {
     try {
         await mongoose.connect(url);
         console.log("Connected to MongoDB");
     } catch (err) {
         console.error("Failed to connect to MongoDB:", err);
     }
-};
+}
 
 module.exports = connectDB;
